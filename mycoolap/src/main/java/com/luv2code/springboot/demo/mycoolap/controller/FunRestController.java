31584c5ea690f6159dd9b458db4d579190589732
lion772 <williamlopes64@gmail.com>
2023-07-26 16:16:27 +0200

    add devtool dependency to pom.xml

--- mycoolap/src/main/java/com/luv2code/springboot/demo/mycoolap/controller/FunRestController.java
+++ mycoolap/src/main/java/com/luv2code/springboot/demo/mycoolap/controller/FunRestController.java
@@ -1,5 +1,6 @@
 package com.luv2code.springboot.demo.mycoolap.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,9 @@ import java.util.List;
 
 @RestController
 public class FunRestController {
+
+    @Value("${person.name}")
+    private String name;
 
     private List<String> strings = new ArrayList<String>();
 
