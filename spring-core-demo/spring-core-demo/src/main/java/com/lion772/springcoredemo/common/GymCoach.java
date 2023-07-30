d31d4beb0d365aaa99d843beb7c44575844f2c67
lion772 <williamlopes64@gmail.com>
2023-07-30 18:45:03 +0200

    add setter injection, add @Qualifier to switch implementation

---
+++ spring-core-demo/spring-core-demo/src/main/java/com/lion772/springcoredemo/common/GymCoach.java
@@ -0,0 +1,11 @@
+package com.lion772.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component("gymDao")
+public class GymCoach implements Coach {
+    @Override
+    public String getDailyActivities() {
+        return "Do push-ups, run on treadmill and finish with bench-press!!!";
+    }
+}
