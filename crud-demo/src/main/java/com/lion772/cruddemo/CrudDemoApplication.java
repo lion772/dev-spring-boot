5ddc8dcf317f0415cb8f06abc6861713c1c7b4c4
lion772 <williamlopes64@gmail.com>
2023-08-18 11:50:55 +0200

    create Stundent Entity Class to map to db, make small changes in application.yml

--- crud-demo/src/main/java/com/lion772/cruddemo/CrudDemoApplication.java
+++ crud-demo/src/main/java/com/lion772/cruddemo/CrudDemoApplication.java
@@ -1,13 +1,25 @@
 package com.lion772.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudDemoApplication.class, args);
+	}
+
+	// This little snippet of code will be executed after the beans have been loaded
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		//Java lambda expression -> shorthand notation for creating an implementation for CommandLineRunner
+		return runner -> {
+			System.out.println("Hello World");
+		};
 	}
 
 }
